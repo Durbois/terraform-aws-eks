4486078c7d8d495b6d8e5b0d4aca0ce96b9babe1
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-02-09 05:55:22 +0100

    vpc cidr changed

--- examples/eks_managed_node_group/main.tf
+++ examples/eks_managed_node_group/main.tf
@@ -10,7 +10,7 @@ locals {
   cluster_version = "1.27"
   region          = "eu-central-1"
 
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "192.168.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
